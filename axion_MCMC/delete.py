a827f38480654869ad537f4131471cb6be853ba2
saracha413 <svannah@wellesley.edu>
2021-10-22 14:17:52 -0400

    Added param boundaries

--- axion_MCMC/delete.py
+++ axion_MCMC/delete.py
@@ -27,11 +27,17 @@ def func(num_it):
 	l_min = 90
 	l_max = 2000
 	n_axion = 3
-	log10_axion_ac = -3.531
-	log10_fraction_axion_ac = -0.879
-	omega_cdm = 0.132
-	H0 = 72.81
-	print('entered MCMC func')
+	#log10_axion_ac = -3.531
+	#log10_fraction_axion_ac = -0.879
+	#omega_cdm = 0.132
+	#H0 = 72.81
+
+
+	#set min and max for sampled params
+	#note this kind of setting a prior :(
+	param_ranges = {'log10_axion_ac':[-5, -3], 'log10_fraction_axion_ac': [-1, -0.1], 'omega_cdm': [0.08, 0.2], 'H0': [60, 90]}
+
+
 
 	l_min, l_max = 90, 2000
 	model_pars = read_ini_file('example_axiCLASS.ini', loc='/Users/saravannah/Axion-MCMC/axion_MCMC/')
@@ -67,9 +73,14 @@ def func(num_it):
 	
 	#start = time.time()
 
-	for i in range(num_it):
+	i = 0
+
+	#for i in range(num_it):
+	while i < num_it:
+
 
 		p_propose = np.random.normal(p_current, stdDevs)
+
 
         ####TO-DO: write this fxn to use whatever variable param you want. hard-coded for now
 		model_pars['log10_axion_ac'] = p_propose[0]
@@ -79,27 +90,44 @@ def func(num_it):
 
 
 
-		#log10_axion_ac = np.random.normal(log10_axion_ac, abs(log10_axion_ac*0.1))
-		#log10_fraction_axion_ac = np.random.normal(log10_fraction_axion_ac, abs(log10_fraction_axion_ac*0.1))
-		#omega_cdm = np.random.normal(omega_cdm, abs(omega_cdm*0.05))
-		#H0 = np.random.normal(H0, abs(H0*0.1))
-		try:
-			with timeout(60, exception=RuntimeError):
-				_, _, Dl_propose = get_power(model_pars, l_min, l_max)
-		except (RuntimeError, CosmoComputationError):
-			pass
-			#print('didnt finish within 5 seconds')
-
-		JSD_propose = JSD(Dl_propose, Dl_data)
-		x = JSD_propose/JSD_current
-
-		if x < 1+np.random.uniform():
-			p_current = p_propose
-			JSD_current = JSD_propose
+		#NOTE: you can organize this more pythonically by changing param_range to be a dict and using the key from model_pars
+		allParamsWithinRange = True
+		for param in param_ranges:
+			if model_pars[param] < param_ranges[param][0] or model_pars[param] > param_ranges[param][1]:
+				allParamsWithinRange = False
 
 
-		line = np.append(p_current, JSD_current)
-		sampling_result.append(line)
+		if allParamsWithinRange:
+			#log10_axion_ac = np.random.normal(log10_axion_ac, abs(log10_axion_ac*0.1))
+			#log10_fraction_axion_ac = np.random.normal(log10_fraction_axion_ac, abs(log10_fraction_axion_ac*0.1))
+			#omega_cdm = np.random.normal(omega_cdm, abs(omega_cdm*0.05))
+			#H0 = np.random.normal(H0, abs(H0*0.1))
+			try:
+				with timeout(60, exception=RuntimeError):
+					_, _, Dl_propose = get_power(model_pars, l_min, l_max)
+			#except (RuntimeError, CosmoComputationError):
+			#	pass
+			#	#print('didnt finish within 5 seconds')
+			except RuntimeError:
+				pass
+
+			#except CosmoComputationError:
+			#	print('Step number ', i)
+			#	print('Param values for log10_axion_ac, log10_fraction_axion_ac, omega_cdm, and H0 are', p_propose[0], p_propose[1], p_propose[2], p_propose[3])
+
+			JSD_propose = JSD(Dl_propose, Dl_data)
+			x = JSD_propose/JSD_current
+
+			if x < 1+np.random.uniform():
+				p_current = p_propose
+				JSD_current = JSD_propose
+
+
+			line = np.append(p_current, JSD_current)
+			sampling_result.append(line)
+
+			i=i+1
+
 
 	return sampling_result
 
@@ -108,12 +136,17 @@ if __name__ == '__main__':
 	num_chains = 10
 	num_steps = 200
 	saveFile = True
-	fileName = 'Oct-20.txt'
+	#if saveFile:
+	fileName = 'Oct-22.txt'
+
+
 
 	print('starting code')
 
 	model_pars = read_ini_file('example_axiCLASS.ini', loc='/Users/saravannah/Axion-MCMC/axion_MCMC/')
 	model_pars['n_axion'] = 3
+
+
 
 
 	pool = mp.Pool(processes=24)
@@ -122,10 +155,12 @@ if __name__ == '__main__':
 	n_trials_per_process = [num_steps] * num_chains
 	#result = pool.map(func, n_trials_per_process)
 	#_, _, Dls = get_power(model_pars, 0, 2000)
+
 	total_sampling_result = pool.map(func, n_trials_per_process)
 	#result = pool.map(mcmc, n_trials_per_process)
 	pool.close()
 	print('The pool is now closed.')
+
 
 	#if saveFile:
 	#	for i in range(num_chains):
@@ -140,7 +175,9 @@ if __name__ == '__main__':
 	H0 = np.zeros((num_chains, len(total_sampling_result[0])))
 	Djs = np.zeros((num_chains, len(total_sampling_result[0])))
 	
+
 	for i in range(num_chains):
+
 		log10_axion_ac[i] = [col[0] for col in total_sampling_result[i]] #extract column
 		log10_fraction_axion_ac[i] = [col[1] for col in total_sampling_result[i]]
 		omega_cdm[i] = [col[2] for col in total_sampling_result[i]]
@@ -148,23 +185,7 @@ if __name__ == '__main__':
 		Djs[i] = [col[4] for col in total_sampling_result[i]]
 		chain_arr = np.array([log10_axion_ac[i], log10_fraction_axion_ac[i], omega_cdm[i], H0[i], Djs[i]])
 		chainFileName = fileName.split('.tx')[0] + '#'+str(i)+'.txt'
-		np.savetxt(chainFileName, chain_arr)
+		np.savetxt(chainFileName, np.transpose(chain_arr))
 
 
-
-
-
-#	if saveFile:
-#	#create total array to combine all the chain data
-#		big_arr = np.zeros((5, len(log10_axion_ac[0])*num_chains))
-#
-#		big_arr[0] = np.concatenate(([log10_axion_ac[i] for i in range(num_chains)]))
-#		big_arr[1] = np.concatenate(([log10_fraction_axion_ac[i] for i in range(num_chains)]))
-#		big_arr[2] = np.concatenate(([omega_cdm[i] for i in range(num_chains)]))
-#		big_arr[3] = np.concatenate(([H0[i] for i in range(num_chains)]))
-#		big_arr[4] = np.concatenate(([Djs[i] for i in range(num_chains)]))
-#		#save run to file
-#		with open(fileName, 'a') as fileObject:
-#			np.savetxt(fileObject, np.transpose(big_arr))
-                
            
