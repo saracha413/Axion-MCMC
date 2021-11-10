b78a0124cd649029689fb55198e052529e7cc0ff
saracha413 <svannah@wellesley.edu>
2021-11-10 14:31:17 -0500

    updated bounds and removed redundancies

--- axion_MCMC/TheGoldenMCMC.py
+++ axion_MCMC/TheGoldenMCMC.py
@@ -9,15 +9,15 @@ from classy import CosmoComputationError
 import os, os.path, copy
 
 from interruptingcow import timeout
+import tqdm
 
 
-#let 
-def change_starting_place(pars_list):
+#NOTE IF THIS WORKS YOU CAN REMOVE PARS_LIST FROM ARGUMENTS!!!!
+def change_starting_place(pars_list, bounds):
 
 	new_pars_list = np.zeros(len(pars_list))
 	for i in range(len(pars_list)):
-		param = float(pars_list[i])
-		new_pars_list[i] = np.random.normal(param, abs(param*0.1)) 
+		new_pars_list[i] = np.random.uniform(bounds[i][0], bounds[i][1]) 
 
 	return new_pars_list
 
@@ -26,35 +26,28 @@ def func(num_it):
 	num_burn_in = 0
 	l_min = 90
 	l_max = 2000
-	n_axion = 3
-	#log10_axion_ac = -3.531
-	#log10_fraction_axion_ac = -0.879
-	#omega_cdm = 0.132
-	#H0 = 72.81
+	n_axion = 2
 
 
 	#set min and max for sampled params
-	#note this kind of setting a prior :(
-	param_ranges = {'log10_axion_ac':[-5, -3], 'log10_fraction_axion_ac': [-1, -0.1], 'omega_cdm': [0.08, 0.2], 'H0': [60, 90]}
+	#note this kind of setting a prior :
+	param_ranges =  {'log10_axion_ac':[-4.6, -3], 'log10_fraction_axion_ac': [-2, -0.82], 'omega_cdm': [0.095, 0.15], 'H0': [65, 80]}
 
 
-
-	l_min, l_max = 90, 2000
 	model_pars = read_ini_file('example_axiCLASS.ini', loc='/Users/saravannah/Axion-MCMC/axion_MCMC/')
-	model_pars['n_axion'] = 3
+	model_pars['n_axion'] = n_axion
 
-	#log10_axion_ac = float(model_pars['log10_axion_ac'])
-	#log10_fraction_axion_ac = float(model_pars['log10_fraction_axion_ac'])
-	#omega_cdm = float(model_pars['omega_cdm'])
-	#H0 = float(model_pars['H0'])
 	sampling_result = []
 
 
+
 	p_current = [model_pars['log10_axion_ac'], model_pars['log10_fraction_axion_ac'], model_pars['omega_cdm'], model_pars['H0']] #whatever params you're varying in MCMC
+	stdDevs = [str(abs(float(x)*0.1)) for x in p_current]
 
 	#assign new starting places for each chain
-	p_current = change_starting_place(p_current)
-	stdDevs = [str(abs(float(x)*0.1)) for x in p_current]
+	#note since p_current is an array not a dict, have to convert valeus in param_ranges to a list as well
+	p_current = change_starting_place(p_current, list(param_ranges.values()))
+	
 
 
 	model_pars['log10_axion_ac'] = p_current[0]
@@ -68,16 +61,17 @@ def func(num_it):
 	_, Dl_data, _, _ = np.loadtxt(fileName, unpack = True)
 	Dl_data = Dl_data[l_min-2:l_max-1] #truncation checked to match l_max and l_min
 
+
 	_, _, Dl_init = get_power(model_pars, l_min, l_max)
 	JSD_current = JSD(Dl_init, Dl_data)
 	
 	#start = time.time()
 
 	i = 0
+	num_accept = 0 #number of steps accepted
 
 	#for i in range(num_it):
 	while i < num_it:
-
 
 		p_propose = np.random.normal(p_current, stdDevs)
 
@@ -98,22 +92,17 @@ def func(num_it):
 
 
 		if allParamsWithinRange:
-			#log10_axion_ac = np.random.normal(log10_axion_ac, abs(log10_axion_ac*0.1))
-			#log10_fraction_axion_ac = np.random.normal(log10_fraction_axion_ac, abs(log10_fraction_axion_ac*0.1))
-			#omega_cdm = np.random.normal(omega_cdm, abs(omega_cdm*0.05))
-			#H0 = np.random.normal(H0, abs(H0*0.1))
+
 			try:
 				with timeout(60, exception=RuntimeError):
 					_, _, Dl_propose = get_power(model_pars, l_min, l_max)
-			#except (RuntimeError, CosmoComputationError):
-			#	pass
-			#	#print('didnt finish within 5 seconds')
+
 			except RuntimeError:
 				pass
 
-			#except CosmoComputationError:
-			#	print('Step number ', i)
-			#	print('Param values for log10_axion_ac, log10_fraction_axion_ac, omega_cdm, and H0 are', p_propose[0], p_propose[1], p_propose[2], p_propose[3])
+			except CosmoComputationError:
+				print('Step number ', i)
+				print('Param values for log10_axion_ac, log10_fraction_axion_ac, omega_cdm, and H0 are', p_propose[0], p_propose[1], p_propose[2], p_propose[3])
 
 			JSD_propose = JSD(Dl_propose, Dl_data)
 			x = JSD_propose/JSD_current
@@ -121,6 +110,7 @@ def func(num_it):
 			if x < 1+np.random.uniform():
 				p_current = p_propose
 				JSD_current = JSD_propose
+				num_accept = num_accept + 1
 
 
 			line = np.append(p_current, JSD_current)
@@ -128,7 +118,9 @@ def func(num_it):
 
 			i=i+1
 
+	frac_accepted = num_accept/num_it
 
+	print('Acceptance fraction is ', frac_accepted)
 	return sampling_result
 
 
@@ -137,38 +129,30 @@ if __name__ == '__main__':
 	num_steps = 200
 	saveFile = True
 	#if saveFile:
-	fileName = 'Oct-22.txt'
+	fileName = 'Nov-11.txt'
 
 
 
 	print('starting code')
-
-	model_pars = read_ini_file('example_axiCLASS.ini', loc='/Users/saravannah/Axion-MCMC/axion_MCMC/')
-	model_pars['n_axion'] = 3
-
-
 
 
 	pool = mp.Pool(processes=24)
 	print('The pool is open! Come swim!')
 
 	n_trials_per_process = [num_steps] * num_chains
-	#result = pool.map(func, n_trials_per_process)
-	#_, _, Dls = get_power(model_pars, 0, 2000)
+
 
 	total_sampling_result = pool.map(func, n_trials_per_process)
-	#result = pool.map(mcmc, n_trials_per_process)
+	#total_sampling_result = []
+	#for _ in tqdm.tqdm(pool.imap_unordered(func, n_trials_per_process), total=num_steps*num_chains):
+	#	total_sampling_result.append(_)
+
+
+
 	pool.close()
 	print('The pool is now closed.')
 
 
-	#if saveFile:
-	#	for i in range(num_chains):
-	#
-	#		chainFileName = fileName.split('.tx')[0] + '#'+str(i)+'.txt'
-	#		np.savetxt(total_sampling_result[i],chainFileName)
-	#
-	#
 	log10_axion_ac = np.zeros((num_chains, len(total_sampling_result[0])))
 	log10_fraction_axion_ac = np.zeros((num_chains, len(total_sampling_result[0])))
 	omega_cdm = np.zeros((num_chains, len(total_sampling_result[0])))
